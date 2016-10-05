fdac8566a31ba93e16ef66079b95dd585caee761
Android <sdf>
2016-10-05 16:34:41 +0300

    Initial commit

---
+++ app/src/main/java/com/sa/proxapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.sa.proxapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    View.OnClickListener regButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, RegActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    Button reg_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        reg_button = (Button) findViewById(R.id.reg_button);
+        reg_button.setOnClickListener(regButtonListener);
+    }
+}
